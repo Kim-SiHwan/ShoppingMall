d5b5d920c1fe185fd9c55056057e1d4f44f53b11
Kim-SiHwan <tjdans4166@naver.com>
2020-06-23 21:44:16 +0900

    Order Controller 생성

---
+++ src/main/java/com/example/atelier/controller/OrderController.java
@@ -0,0 +1,12 @@
+package com.example.atelier.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RequiredArgsConstructor
+@Controller
+@RequestMapping("/shop")
+public class OrderController {
+
+}
