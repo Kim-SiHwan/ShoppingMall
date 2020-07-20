157dad2e004ba619e6c74c65f3ec848be0bc3484
Kim-SiHwan <tjdans4166@naver.com>
2020-07-20 21:22:37 +0900

    주문 초기

--- src/main/java/com/example/atelier/mapper/OrderMapper.java
+++ src/main/java/com/example/atelier/mapper/OrderMapper.java
@@ -1,8 +1,15 @@
 package com.example.atelier.mapper;
 
+import com.example.atelier.domain.Member;
+import com.example.atelier.domain.Order;
+import com.example.atelier.domain.Product;
 import org.springframework.stereotype.Repository;
 
 @Repository("com.example.atelier.mapper.OrderMapper")
-public class OrderMapper {
+public interface OrderMapper {
+
+    public Product getProductInfo(Long pid);
+    public Member getUserInfo(String uid);
+    public int orderInput(Order order);
 
 }
